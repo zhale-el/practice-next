ba51c54a8351f55ee0d1441de2873503b9187b49
zhaleh <zhalehelmi@gmail.com>
2024-05-29 21:06:09 +0330

    init

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import SwrComponent from "../components/componentSwr";
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-4xl"> Home page</h1>
+      <div>
+        <SwrComponent />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
